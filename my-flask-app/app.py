de59065cafd916e9808fdfc9bde1cff30cf224f9
basu-asesh <127032994+basu-asesh@users.noreply.github.com>
2025-08-28 03:54:25 +0530

    Update app.py

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,5 +12,3 @@ def f1():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
